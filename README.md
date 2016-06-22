4c02cdea8ef38b9bb21bbc035ad2b7130e3e3488
jrivera <jrivera@brightcove.com>
2016-06-22 18:10:44 -0400

    Add a bin file to enable comamnd-line decryption

--- README.md
+++ README.md
@@ -12,6 +12,12 @@
 npm install --save aes-decrypter
 ```
 
+Also available to install globally:
+
+```sh
+npm install --global aes-decrypter
+```
+
 The npm installation is preferred, but Bower works, too.
 
 ```sh
@@ -32,24 +38,48 @@ var keyContent = fs.readFileSync(keyFile);
 
 var view = new DataView(keyContent.buffer);
 var key.bytes = new Uint32Array([
-	view.getUint32(0),
-	view.getUint32(4),
-	view.getUint32(8),
-	view.getUint32(12)
+  view.getUint32(0),
+  view.getUint32(4),
+  view.getUint32(8),
+  view.getUint32(12)
 ]);
 
 key.iv = new Uint32Array([
-	0, 0, 0, 0
+  0, 0, 0, 0
 ]);
 
 var d = new Decrypter(
-	encryptedBytes,
-	key.bytes,
-	key.iv,
-	function(err, decryptedBytes) {
-		// err always null
+  encryptedBytes,
+  key.bytes,
+  key.iv,
+  function(err, decryptedBytes) {
+    // err always null
 });
+```
 
+## Command Line Usage
+
+Using this module as a command is as easy as:
+```bash
+$ aes-decrypter
+  Usage: aes-decrypter [options] <input file> [output file]
+
+  Options:
+
+    -h, --help     output usage information
+    -k, --key <n>  The keyfile
+    -i, --iv <n>   The initialization vector (0x<256bit hex string>) or a file containing an IV
+```
+
+Examples:
+```bash
+# Decrypt encrypted.bin with a keyfile and an initialization vector provided
+# on the command-line saving the output to decrypted.bin
+aes-decrypter -k encrypted.key -i 0x00000000000000000000000000000001 encrypted.bin decrypted.bin
+
+# Decrypt encrypted.bin with a keyfile and an initialization vector file
+# outputing the data to stdout
+aes-decrypter -k encrypted.key -i encrypted.iv encrypted.bin
 ```
 
 ## License
---
+++ bin/index.js
@@ -0,0 +1,69 @@
+#!/usr/bin/env node
+'use strict';
+
+let Decrypter = require('../').Decrypter;
+let commander = require('commander');
+let fs = require('fs');
+let path = require('path');
+
+let parseIV = (opt) => {
+  let ivContent;
+  let ivPath = path.resolve(opt);
+  if (fs.existsSync(ivPath)) {
+    ivContent = fs.readFileSync(ivPath).toString();
+  } else {
+    ivContent = opt;
+  }
+  let ints = ivContent.match(/^0?x?(.{8})(.{8})(.{8})(.{8})/i);
+
+  let iv = new Uint32Array([
+      parseInt(ints[1], 16),
+      parseInt(ints[2], 16),
+      parseInt(ints[3], 16),
+      parseInt(ints[4], 16)
+  ]);
+
+  return iv;
+};
+
+commander
+  .usage('[options] <input file> [output file]')
+  .option('-k, --key <n>', 'The keyfile')
+  .option('-i, --iv <n>', 'The initialization vector (256bit hex-string) or a file containing an IV', parseIV)
+  .parse(process.argv);
+
+if (commander.args.length < 1 ||
+    typeof commander.key === undefined ||
+    typeof commander.iv === undefined) {
+  commander.outputHelp();
+  process.exit(1);
+}
+
+let keyContent = fs.readFileSync(path.resolve(commander.key));
+let encryptedBytes = fs.readFileSync(path.resolve(commander.args[0]));
+
+let key = new Uint32Array([
+  keyContent.readUInt32BE(0),
+  keyContent.readUInt32BE(4),
+  keyContent.readUInt32BE(8),
+  keyContent.readUInt32BE(12)
+]);
+
+let d = new Decrypter(
+  new Uint8Array(encryptedBytes),
+  key,
+  commander.iv,
+  function(err, decryptedBytes) {
+    // err always null
+    if (!err) {
+      let data = new Buffer(decryptedBytes);
+
+      if (commander.args.length > 1) {
+        // If we have a filename to write to, do it
+        fs.writeFileSync(path.resolve(commander.args[1]), data);
+      } else {
+        // Otherwise, just output the data to stdout
+        process.stdout.write(data);
+      }
+    }
+});
